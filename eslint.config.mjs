afed2823eb54b0b7f9f1443c8e7af8bd4933d585
Amir Hossein Nasrollahi <ah12.nasrollahi@gmail.com>
2025-06-02 09:57:49 +0330

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from '@fullstacksjs/eslint-config'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default defineConfig({
+  typescript: {
+    tsconfigRootDir: import.meta.dirname, // If you are using TypeScript
+  },
+  rules: {},
+  prettier: 0,
+})
